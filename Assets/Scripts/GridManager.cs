82ed002ce634d70f9204ea07e4b5affe7403694f
Moritz <moritzkuebler0505@gmail.com>
2026-01-30 12:24:38 +0100

    Add data model for grid and input line

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    private bool[] inputLineData = new bool[7];
+
+    private bool[,] gridData = new bool[10, 7];
+
+    void Start()
+    {
+        
+    }
+}
