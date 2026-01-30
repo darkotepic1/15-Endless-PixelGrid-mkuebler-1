d8c009710ed63b53329dc95ba9fd97ef4a7c97bd
Moritz <moritzkuebler0505@gmail.com>
2026-01-30 12:28:31 +0100

    Implement toggle logic for input line pixels

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -10,4 +10,30 @@ public class GridManager : MonoBehaviour
     {
         
     }
+
+    // ... (Datenmodell Code von vorher)
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {   
+        if (Input.GetKeyDown(KeyCode.W)) ToggleInputPixel(0);          // Q6
+        if (Input.GetKeyDown(KeyCode.A)) ToggleInputPixel(1);          // Q5
+        if (Input.GetKeyDown(KeyCode.UpArrow)) ToggleInputPixel(2);    // Q4
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) ToggleInputPixel(3);  // Q3
+        if (Input.GetKeyDown(KeyCode.DownArrow)) ToggleInputPixel(4);  // Q2
+        if (Input.GetKeyDown(KeyCode.RightArrow)) ToggleInputPixel(5); // Q1
+        if (Input.GetKeyDown(KeyCode.S)) ToggleInputPixel(6);          // Q0
+    }
+
+    private void ToggleInputPixel(int index)
+    {
+        if (index >= 0 && index < inputLineData.Length)
+        {
+            inputLineData[index] = !inputLineData[index];
+    }
+    }
 }
